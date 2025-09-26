cb8a8a23cc752b2863c1ed9b0196c2cb86de93eb
m4yank <m4yank.t@gmail.com>
2025-09-27 01:54:21 +0530

    reverted

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
-import React, { useState, useRef } from 'react'
 import LandingPage from "./sections/LandingPage"
 import EventsHome from "./sections/events"
-import Whatisincub8 from "./sections/whatisincub8"
 
 function App() {
-  const [isVideoPaused, setIsVideoPaused] = useState(false)
-  const videoRef = useRef(null)
-
   return (
     <>
-      <LandingPage 
-        videoRef={videoRef}
-        isVideoPaused={isVideoPaused}
-        setIsVideoPaused={setIsVideoPaused}
-      />
-      <Whatisincub8 
-        setIsVideoPaused={setIsVideoPaused}
-        videoRef={videoRef}
-      />
+      <LandingPage />
       <EventsHome />
     </>
   )
