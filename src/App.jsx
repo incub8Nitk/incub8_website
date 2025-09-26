660f746d6a8590dab409139f55dab0fc398562b8
m4yank <m4yank.t@gmail.com>
2025-09-27 01:37:52 +0530

    added the What is incub8 section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import React, { useState, useRef } from 'react'
 import LandingPage from "./sections/LandingPage"
 import EventsHome from "./sections/events"
+import Whatisincub8 from "./sections/whatisincub8"
 
 function App() {
+  const [isVideoPaused, setIsVideoPaused] = useState(false)
+  const videoRef = useRef(null)
+
   return (
     <>
-      <LandingPage />
+      <LandingPage 
+        videoRef={videoRef}
+        isVideoPaused={isVideoPaused}
+        setIsVideoPaused={setIsVideoPaused}
+      />
+      <Whatisincub8 
+        setIsVideoPaused={setIsVideoPaused}
+        videoRef={videoRef}
+      />
       <EventsHome />
     </>
   )
