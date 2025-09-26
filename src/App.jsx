da6cd650fdccb4f450c527d8b1801c35e541e721
Mayank Tiwari <m4yank.t@gmail.com>
2025-09-27 02:38:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,10 @@ import EventsHome from "./sections/events"
 import Whatisincub8 from "./sections/whatisincub8"
 
 function App() {
-  const [isVideoPaused, setIsVideoPaused] = useState(false)
-  const videoRef = useRef(null)
-
   return (
     <>
-      <LandingPage 
-        videoRef={videoRef}
-        isVideoPaused={isVideoPaused}
-        setIsVideoPaused={setIsVideoPaused}
-      />
-      <Whatisincub8 
-        setIsVideoPaused={setIsVideoPaused}
-        videoRef={videoRef}
-      />
+      <LandingPage />
+      <Whatisincub8 />
       <EventsHome />
     </>
   )
