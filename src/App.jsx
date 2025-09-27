b44e7e6cbc0e288af2be827c38a989895aa82f29
m4yank <m4yank.t@gmail.com>
2025-09-27 10:41:33 +0530

    local changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
+import React, { useState, useRef } from 'react'
 import LandingPage from "./sections/LandingPage"
 import EventsHome from "./sections/events"
+import Whatisincub8 from "./sections/whatisincub8"
 
 function App() {
+
+
   return (
     <>
       <LandingPage />
-<<<<<<< HEAD
-=======
       <Whatisincub8 />
->>>>>>> aboutandpast
       <EventsHome />
     </>
   )
